c507c5b7cfe177233f0a1e9b16197fc174fbba81
Gaurav Rai <gaurav.rai@team.telstra.com>
2023-06-24 19:27:20 +1000

    my while loop

---
+++ src/learningJava/whileLoop.java
@@ -0,0 +1,15 @@
+package learningJava;
+
+public class whileLoop {
+    public static void main(String[] args) {
+        System.out.println("Before Loop");
+
+        int x=4;
+
+        while ( x>0){
+            System.out.println(" I am in a while loop" + " " +x);
+           x--;
+        }
+
+    }
+}
