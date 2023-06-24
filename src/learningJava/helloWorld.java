b751b86bfdd5344fd74341b0795f96319d8d2b00
Gaurav Rai <gaurav.rai@team.telstra.com>
2023-06-24 19:12:26 +1000

    my first java program

---
+++ src/learningJava/helloWorld.java
@@ -0,0 +1,12 @@
+package learningJava;
+
+import java.net.StandardSocketOptions;
+
+public class helloWorld {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("learning java for automation");
+    }
+}
