c437a003e7efe70a459ea46727d4a6a73c1af143
Gaurav Rai <gaurav.rai@team.telstra.com>
2023-06-30 11:45:09 +1000

    string tutorial

---
+++ src/learningJava/strtutorial.java
@@ -0,0 +1,20 @@
+package learningJava;
+
+public class strtutorial {
+
+    public static void main(String[] args) {
+
+        String hello = "my name is Gaurav";
+
+      int  hellolength= hello.length();
+
+      System.out.println(hellolength);
+
+      char charat=hello.charAt(1);
+
+        System.out.println(charat);
+
+        String uCase=hello.toUpperCase();
+        System.out.println(uCase);
+    }
+}
